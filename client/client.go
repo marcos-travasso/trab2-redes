223840f3082fb2f42079a066d7f281b17e7e3627
Marcos Travasso <marcos.travasso42@gmail.com>
2023-11-15 16:43:09 -0300

    se concretizando meodeos me perdoe por esses commits ruins

--- client/client.go
+++ client/client.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"encoding/binary"
+	"fmt"
 	"log"
+	"math"
 	"net"
 
 	"golang.org/x/sys/unix"
 )
+
+const BUFFER_SIZE = 2048
+
+type Metadata struct {
+	FileSize uint32
+	MD5Hash  []byte
+	FileName string
+}
 
 func main() {
 	// Create UDP socket
@@ -24,7 +35,7 @@ func main() {
 	copy(serverAddr.Addr[:], net.ParseIP("127.0.0.1").To4())
 
 	// Message to send
-	message := []byte("Hello, server!")
+	message := []byte("GET hello.txt")
 
 	// Send the message to the server
 	err = unix.Sendto(clientFd, message, 0, serverAddr)
@@ -34,12 +45,64 @@ func main() {
 
 	log.Printf("Message sent to the server: %s\n", string(message))
 
-	// Receive the response from the server
-	buffer := make([]byte, 1024)
-	n, _, err := unix.Recvfrom(clientFd, buffer, 0)
+	received := make(map[uint32][]byte)
+	totalBlocks := 65535
+	for len(received) != totalBlocks {
+
+		// Receive the response from the server
+		buffer := make([]byte, BUFFER_SIZE)
+		n, _, err := unix.Recvfrom(clientFd, buffer, 0)
+		if err != nil {
+			log.Fatal("Error receiving response from server:", err)
+		}
+
+		if n < 4 {
+			continue
+		}
+
+		if binary.BigEndian.Uint32(buffer[:4]) == 0 {
+			totalBlocks = handleMetadata(buffer)
+			log.Printf("DEBUG Total de blocos: %d\n", totalBlocks)
+			continue
+		}
+
+		handleBlock(buffer[:n], received)
+	}
+}
+
+func handleBlock(buffer []byte, received map[uint32][]byte) {
+	position := binary.BigEndian.Uint32(buffer[:4])
+	received[position] = make([]byte, len(buffer))
+	copy(received[position], buffer)
+	log.Printf("DEBUG Bloco %d recebeu: %x\n", position, received[position])
+}
+
+func handleMetadata(buffer []byte) int {
+	metadata, err := readMetadataByteArray(buffer)
 	if err != nil {
-		log.Fatal("Error receiving response from server:", err)
+		log.Fatal("Error reading metadata:", err)
 	}
 
-	log.Printf("Received %d bytes from server: %s\n", n, string(buffer[:n]))
+	fmt.Println("-------------------------------------")
+	fmt.Printf("File Size: %d\n", metadata.FileSize)
+	fmt.Printf("MD5 Hash: %x\n", metadata.MD5Hash)
+	fmt.Printf("File Name: %s\n", metadata.FileName)
+	fmt.Println("-------------------------------------")
+	return int(math.Ceil(float64(metadata.FileSize) / 2046))
+}
+
+func readMetadataByteArray(data []byte) (*Metadata, error) {
+	fileSize := binary.BigEndian.Uint32(data[4:8])
+
+	md5Hash := make([]byte, 16)
+	copy(md5Hash, data[8:24])
+
+	// Read file name
+	fileName := string(data[24:])
+
+	return &Metadata{
+		FileSize: fileSize,
+		MD5Hash:  md5Hash,
+		FileName: fileName,
+	}, nil
 }
--- server/server.go
+++ server/server.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"math"
 	"net"
+	"os"
+	"strings"
 
 	"golang.org/x/sys/unix"
 )
 
+var PORT = 8080
+var serverFd int
+var BUFFER_SIZE = 2048
+
+var COMMAND_ERROR = errors.New("Command error")
+var FILE_ERROR = errors.New("File error")
+
 func main() {
-	// Create UDP socket
-	serverFd, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
+	var err error
+	serverFd, err = unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
 	if err != nil {
 		log.Fatal("Error creating UDP socket:", err)
 	}
@@ -17,9 +32,8 @@ func main() {
 
 	log.Printf("UDP Socket [%v] created\n", serverFd)
 
-	// Bind the socket to a specific address and port
 	socketAddress := &unix.SockaddrInet4{
-		Port: 8080,
+		Port: PORT,
 	}
 	copy(socketAddress.Addr[:], net.ParseIP("0.0.0.0").To4())
 
@@ -29,8 +43,11 @@ func main() {
 	}
 	log.Printf("UDP Socket binded to port %v\n", socketAddress.Port)
 
-	// Handle incoming data
-	buffer := make([]byte, 1024)
+	listen()
+}
+
+func listen() {
+	buffer := make([]byte, BUFFER_SIZE)
 	for {
 		n, addr, err := unix.Recvfrom(serverFd, buffer, 0)
 		if err != nil {
@@ -38,17 +55,145 @@ func main() {
 			continue
 		}
 
-		clientAddr := addr.(*unix.SockaddrInet4)
+		handleClient(addr.(*unix.SockaddrInet4), n, buffer)
+	}
+}
 
-		log.Printf("Received %d bytes from %+v: %s\n", n, clientAddr, string(buffer[:n]))
+func handleClient(clientAddr *unix.SockaddrInet4, n int, buffer []byte) {
+	client := getClientName(clientAddr)
+	log.Printf("%s > %s\n", client, string(buffer[:n]))
 
-		// Respond to the client
-		response := []byte("Hello, client!")
-		err = unix.Sendto(serverFd, response, 0, &unix.SockaddrInet4{
-			Port: clientAddr.Port,
-		})
+	responses, err := handleCommand(string(buffer[:n]))
+	if err != nil {
+		log.Printf("Error handling client %s: %v\n", client, err)
+		return
+	}
+	log.Printf("DEBUG sending %d blocks to %s\n", len(responses), client)
+
+	for _, response := range responses {
+		log.Printf("sending: %x (len %d)\n", response, len(response))
+		err = unix.Sendto(serverFd, response, 0, &unix.SockaddrInet4{Port: clientAddr.Port})
 		if err != nil {
-			log.Println("Error writing to UDP socket:", err)
+			log.Printf("Error writing to %s: %v\n", client, err)
+			return
 		}
 	}
+}
+
+func handleCommand(command string) ([][]byte, error) {
+	parts := strings.Split(command, " ")
+	if len(parts) < 2 || parts[0] != "GET" {
+		return nil, COMMAND_ERROR
+	}
+
+	if len(parts) > 3 {
+		return handleSpecificGet(command)
+	}
+
+	return handleGet(parts)
+}
+
+func handleGet(parts []string) ([][]byte, error) {
+	file, err := os.Open("./" + parts[1])
+	s, err2 := file.Stat()
+	if err != nil || err2 != nil || s.IsDir() {
+		return nil, FILE_ERROR
+	}
+	defer file.Close()
+
+	metadata, err := createMetadataByteArray(file)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := createDataByteArray(file, s.Size())
+	if err != nil {
+		return nil, err
+	}
+
+	return joinBytes(metadata, data), nil
+}
+
+func handleSpecificGet(command string) ([][]byte, error) {
+	return nil, nil
+}
+
+func createMetadataByteArray(file *os.File) ([]byte, error) {
+	// Get file size
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	fileSize := uint32(fileInfo.Size())
+
+	// Get MD5 hash of the file content
+	md5Sum, err := calculateMD5(file)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create byte array
+	data := make([]byte, BUFFER_SIZE)
+
+	binary.BigEndian.PutUint32(data[0:4], 0)
+	binary.BigEndian.PutUint32(data[4:8], fileSize)
+
+	// Set next 128 bits to file MD5 hash
+	copy(data[8:24], md5Sum)
+
+	// Set the rest with the filename, padding with null bytes if needed
+	filenameBytes := []byte(fileInfo.Name())
+	copy(data[24:], filenameBytes)
+
+	return data[:24+len(filenameBytes)], nil
+}
+
+func createDataByteArray(file *os.File, size int64) ([]byte, error) {
+	file.Seek(0, 0)
+
+	data := make([]byte, size)
+	_, err := file.Read(data)
+	return data, err
+}
+
+func joinBytes(metadata, data []byte) [][]byte {
+	dataSize := len(data)
+	totalDataBlocks := int(math.Ceil(float64(dataSize)/2046)) + 1
+	blocks := make([][]byte, totalDataBlocks)
+	log.Printf("size: %d, totaldata %d, blocks %d\n", dataSize, totalDataBlocks, blocks)
+
+	blocks[0] = metadata
+	log.Printf("METADATA %x\n", blocks[0])
+	i := 1
+	for dataSize >= BUFFER_SIZE-2 {
+		log.Printf("writing inside loop")
+		blocks[i] = make([]byte, BUFFER_SIZE)
+		binary.BigEndian.PutUint32(blocks[i][:2], uint32(i))
+		copy(blocks[i][2:], data[(i-1)*(BUFFER_SIZE-2):(i-1)*(BUFFER_SIZE-2)+BUFFER_SIZE-2])
+		i++
+		dataSize -= BUFFER_SIZE - 2
+	}
+
+	if dataSize > 0 {
+		log.Printf("writing in if %d with i %d\n", dataSize, i)
+		blocks[i] = make([]byte, dataSize+4)
+		log.Printf("create datasize (%d ou %d) %d\n", i, uint32(i), dataSize+4)
+		binary.BigEndian.PutUint32(blocks[i][:4], uint32(i))
+		copy(blocks[i][4:], data[(i-1)*(BUFFER_SIZE-2):])
+	}
+
+	return blocks
+}
+
+func calculateMD5(file *os.File) ([]byte, error) {
+	hasher := md5.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		return nil, err
+	}
+	log.Printf("DEBUG MD5: %x\n", hasher.Sum(nil))
+	return hasher.Sum(nil), nil
+}
+
+func getClientName(c *unix.SockaddrInet4) string {
+	return fmt.Sprintf("%d.%d.%d.%d:%d", c.Addr[0], c.Addr[1], c.Addr[2], c.Addr[3], c.Port)
 }
